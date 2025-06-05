bbf39d3ca85d69be38ad9212a1f7148d2fdf7978
unknown <edumwas92@gmail.com>
2025-06-05 05:36:56 +0300

    Display a list of posts using a PostCard component.

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,22 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '@/components/layout/Header';
 import PostCard from '@/components/common/PostCard';
 import { type PostProps } from '@/interfaces';
 
-const Posts = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    // Fetch posts from JSONPlaceholder API
-    const fetchPosts = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await response.json();
-      setPosts(data);
-    };
-
-    fetchPosts();
-  }, []);
-
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <div className="p-8">
       <Header />
@@ -33,6 +24,17 @@ const Posts = () => {
       </div>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data,
+    },
+  };
 };
 
 export default Posts;
